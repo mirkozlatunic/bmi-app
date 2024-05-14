da73fade05ad9794f1f2323eb0b5d61aa72b3c40
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-05-13 23:04:36 -0700

    style of hero stage 1 completed

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 import "./index.css";
-import "bootstrap/dist/css/bootstrap.css";
 
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
