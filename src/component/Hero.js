aa6ce93c79c2d6f9b4b8c217edc02342b7704853
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-05-13 19:32:41 -0700

    installed bootstrap

--- src/component/Hero.js
+++ src/component/Hero.js
@@ -3,28 +3,7 @@ import logo from "../assets/images/logo.svg";
 
 import "./Hero.css";
 
-const buttons = ["Metric", "Imperial"];
-const data = ["Height", "Weight"];
-
 const Hero = () => {
-  const renderButton = buttons.map((button) => {
-    return (
-      <label>
-        <input type="radio" />
-        {button}
-      </label>
-    );
-  });
-
-  const yourData = data.map((data) => {
-    return (
-      <label>
-        {data}
-        <input type="number" />
-      </label>
-    );
-  });
-
   return (
     <div className="hero__section">
       <div className="hero__section-left">
@@ -42,8 +21,48 @@ const Hero = () => {
       </div>
       <div className="hero__section-right">
         <h2 className="hero__title-modal">Enter your details below</h2>
-        <div className="hero__radio">{renderButton}</div>
-        <div className="hero__input">{yourData}</div>
+        <div className="hero__data">
+          <div className="hero__radio">
+            <label className="hero__radio-label">
+              <input
+                type="radio"
+                name="selection"
+                className="hero__radio-button"
+              />
+              Metric
+            </label>
+          </div>
+          <div className="hero__radio">
+            <label className="hero__radio-label">
+              <input
+                type="radio"
+                name="selection"
+                className="hero__radio-button"
+              />
+              Imperial
+            </label>
+          </div>
+          <div className="hero__input-text">Height</div>
+          <div className="hero__input-text">Weight</div>
+          <input
+            type="text"
+            name="input"
+            placeholder="0"
+            className="hero__input"
+          />
+          <input
+            type="text"
+            name="input"
+            placeholder="0"
+            className="hero__input"
+          />
+          <div className="hero__result">
+            <h2 className="hero__result-title">Welcome!</h2>
+            <p className="hero__result-text">
+              Enter your height and weight and you’ll see your BMI result here
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
