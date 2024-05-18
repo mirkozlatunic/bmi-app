5a55e6299f710d408f21066b7273758a997e51c2
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-05-17 23:53:38 -0700

    results section desktop version completed

--- src/component/Hero.js
+++ src/component/Hero.js
@@ -6,6 +6,10 @@ import "./Hero.css";
 const Hero = () => {
   const [textValue, setTextValue] = useState("");
   const [numberValue, setNumberValue] = useState("");
+  const [heightValue, setHeightValue] = useState("");
+  const [weightValue, setWeightValue] = useState("");
+  const [bmiValue, setBmiValue] = useState("");
+  const [bmiMessage, setBmiMessage] = useState("");
 
   const textTypeHandler = (e) => {
     if (!e.target.value.match(/\D/g)) setTextValue(e.target.value);
@@ -14,6 +18,31 @@ const Hero = () => {
   const numberTypeHandler = (e) => {
     setNumberValue(e.target.value);
   };
+
+  const calculateBmi = () => {
+    if (heightValue && weightValue) {
+      const heightInMeters = heightValue / 100;
+      const bmi = weightValue(heightInMeters * heightInMeters).toFixed(2);
+      setBmiValue(bmi);
+
+      let message =
+        "Enter your height and weight and you’ll see your BMI result here";
+      if (bmi < 18.5) {
+        message = `${"Your BMI suggests you’re a underweight. Your ideal weight is between 63.3kgs - 85.2kgs."}`;
+      } else if (bmi >= 18.5 && bmi < 24.9) {
+        message = `${"Your BMI suggests you’re a healthy weight. Your ideal weight is between 63.3kgs - 85.2kgs."}`;
+      } else if (bmi >= 24.0 && bmi < 29.9) {
+        message = `${"Your BMI suggests you’re a overweight. Your ideal weight is between 63.3kgs - 85.2kgs."}`;
+      } else {
+        message = `${"Your BMI suggests you’re a obese. Your ideal weight is between 63.3kgs - 85.2kgs."}`;
+      }
+      setBmiMessage(message);
+    } else {
+      setBmiValue("");
+      setBmiMessage("");
+    }
+  };
+
   return (
     <div className="hero__section">
       <div className="hero__section-left">
@@ -38,11 +67,10 @@ const Hero = () => {
                 type="radio"
                 name="selection"
                 className="hero__radio-button"
+                defaultChecked
               />
               Metric
             </label>
-          </div>
-          <div className="hero__radio">
             <label className="hero__radio-label">
               <input
                 type="radio"
@@ -52,28 +80,36 @@ const Hero = () => {
               Imperial
             </label>
           </div>
-          <div className="hero__input-text">Height</div>
-          <div className="hero__input-text">Weight</div>
-          <input
-            type="text"
-            name="input"
-            placeholder="0"
-            className="hero__input"
-            maxLength={3}
-            value={textValue}
-            onChange={textTypeHandler}
-          />
-          <p className="hero__input-uom-left">cm</p>
-          <input
-            type="text"
-            name="input"
-            placeholder="0"
-            className="hero__input"
-            maxLength={3}
-            value={numberValue}
-            onChange={numberTypeHandler}
-          />
-          <p className="hero__input-uom-right">kg</p>
+          <div className="hero__bmi">
+            <div className="hero__entry">
+              <div className="hero__input-text">Height</div>
+              <input
+                type="text"
+                name="input"
+                placeholder="0"
+                className="hero__input"
+                maxLength={3}
+                value={textValue}
+                onChange={textTypeHandler}
+                height={heightValue}
+              />
+              <p className="hero__input-uom-left">cm</p>
+            </div>
+            <div className="hero__entry">
+              <div className="hero__input-text">Weight</div>
+              <input
+                type="text"
+                name="input"
+                placeholder="0"
+                className="hero__input"
+                maxLength={3}
+                value={numberValue}
+                onChange={numberTypeHandler}
+                weight={weightValue}
+              />
+              <p className="hero__input-uom-right">kg</p>
+            </div>
+          </div>
           <div className="hero__result">
             <h2 className="hero__result-title">Welcome!</h2>
             <p className="hero__result-text">
--- src/component/Results.js
+++ src/component/Results.js
@@ -1,7 +1,30 @@
 import React from "react";
+import imageGuy from "../assets/images/image-man-eating.webp";
+import lineLeft from "../assets/images/pattern-curved-line-left.svg";
+
+import "./Results.css";
 
 const Results = () => {
-  return <div>Results</div>;
+  return (
+    <div className="results__container">
+      <img src={imageGuy} alt="guy" className="results__pic" />
+      <div className="results__right">
+        <img src={lineLeft} alt="curve left" className="results__curve-right" />
+        <div className="results__text">
+          <h2 className="results__title">What your BMI result means</h2>
+          <p className="results__para">
+            A BMI range of 18.5 to 24.9 is considered a 'healthy weight.'
+            Maintaining a healthy weight may lower your chances of experiencing
+            health issues later on, such as obesity and type 2 diabetes. Aim for
+            a nutritious diet with reduced fat and sugar content, incorporating
+            ample fruits and vegetables. Additionally, strive for regular
+            physical activity, ideally about 30 minutes daily for five days a
+            week.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Results;
