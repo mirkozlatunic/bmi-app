1c89419ecf59cd7e4f01636f0bf1837305d11f31
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-05-20 09:30:46 -0700

    tips tablet done

--- src/component/Tips.js
+++ src/component/Tips.js
@@ -11,27 +11,33 @@ const Tips = () => {
       <div className="tips__blocks">
         <div className="tips__block">
           <img src={bowl} alt="icons" className="tips__icon" />
-          <h2 className="tips__title">Healthy eating</h2>
-          <p className="tips__text">
-            Healthy eating promotes weight control, disease prevention, better
-            digestion, immunity, mental clarity, and mood.
-          </p>
+          <div className="text__block">
+            <h2 className="tips__title">Healthy eating</h2>
+            <p className="tips__text">
+              Healthy eating promotes weight control, disease prevention, better
+              digestion, immunity, mental clarity, and mood.
+            </p>
+          </div>
         </div>
         <div className="tips__block">
           <img src={dumbell} alt="icons" className="tips__icon" />
-          <h2 className="tips__title">Regular exercise</h2>
-          <p className="tips__text">
-            Exercise improves fitness, aids weight control, elevates mood, and
-            reduces disease risk, fostering wellness and longevity.
-          </p>
+          <div className="text__block">
+            <h2 className="tips__title">Regular exercise</h2>
+            <p className="tips__text">
+              Exercise improves fitness, aids weight control, elevates mood, and
+              reduces disease risk, fostering wellness and longevity.
+            </p>
+          </div>
         </div>
         <div className="tips__block">
           <img src={sleep} alt="icons" className="tips__icon" />
-          <h2 className="tips__title">Adequate sleep</h2>
-          <p className="tips__text">
-            Sleep enhances mental clarity, emotional stability, and physical
-            wellness, promoting overall restoration and rejuvenation.
-          </p>
+          <div className="text__block">
+            <h2 className="tips__title">Adequate sleep</h2>
+            <p className="tips__text">
+              Sleep enhances mental clarity, emotional stability, and physical
+              wellness, promoting overall restoration and rejuvenation.
+            </p>
+          </div>
         </div>
       </div>
     </div>
