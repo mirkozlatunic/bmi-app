981c5894b8eabd4e6f625d0faecbb8faae811111
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-05-19 19:11:09 -0700

    tips section desktop version done

--- src/component/Tips.js
+++ src/component/Tips.js
@@ -1,7 +1,41 @@
 import React from "react";
+import bowl from "../assets/images/icon-eating.svg";
+import dumbell from "../assets/images/icon-exercise.svg";
+import sleep from "../assets/images/icon-sleep.svg";
+
+import "./Tips.css";
 
 const Tips = () => {
-  return <div>Tips</div>;
+  return (
+    <div className="tips__container">
+      <div className="tips__blocks">
+        <div className="tips__block">
+          <img src={bowl} alt="icons" className="tips__icon" />
+          <h2 className="tips__title">Healthy eating</h2>
+          <p className="tips__text">
+            Healthy eating promotes weight control, disease prevention, better
+            digestion, immunity, mental clarity, and mood.
+          </p>
+        </div>
+        <div className="tips__block">
+          <img src={dumbell} alt="icons" className="tips__icon" />
+          <h2 className="tips__title">Regular exercise</h2>
+          <p className="tips__text">
+            Exercise improves fitness, aids weight control, elevates mood, and
+            reduces disease risk, fostering wellness and longevity.
+          </p>
+        </div>
+        <div className="tips__block">
+          <img src={sleep} alt="icons" className="tips__icon" />
+          <h2 className="tips__title">Adequate sleep</h2>
+          <p className="tips__text">
+            Sleep enhances mental clarity, emotional stability, and physical
+            wellness, promoting overall restoration and rejuvenation.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Tips;
